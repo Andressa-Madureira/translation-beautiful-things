78fb79b40e4469f76f86740ebc0a58204d4b4366
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-05-18 14:43:24 -0300

    incluir music

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     var images = [
         "./assets/pexels-tomas-anunziata-129267-3876453.jpg",
-        "./assets/pexels-sheikh-basim-664884875-18419151.jpg", // Adicione o caminho para suas outras imagens aqui
+        "./assets/pexels-sheikh-basim-664884875-18419151.jpg", 
         "./assets/pexels-mododeolhar-5241772.jpg",
         "./assets/pexels-ggzadam-2894948 (1).jpg",
         "./assets/pexels-pixabay-206359.jpg",
         "./assets/pexels-renan-lima-1322164-3364026.jpg",
     ];
     var currentIndex = 0;
-    var duration = 10000; // Intervalo de troca de imagem em milissegundos
+    var duration = 10000; 
 
     var parallax = document.querySelector('.parallax');
 
@@ -22,3 +22,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
     setInterval(changeBackground, duration);
 });
+
+
+const music = new Audio ('./assets/Benson Boone - Beatiful Things (lyrics). (320).mp3') 
+const audioPlayer = document.getElementById("audioPlayer")
+
+audioPlayer.addEventListener('click', playBackgroundMusic)
+
+
+
+
+function playBackgroundMusic() {
+    audioPlayer.play();
+}
+
+function pauseBackgroundMusic() {
+    audioPlayer.pause();
+}
+
+
+playBackgroundMusic();
